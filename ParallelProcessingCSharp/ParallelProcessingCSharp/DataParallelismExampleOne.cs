60f920842f9ae57053cb3024886e45283f7e205b
atmucoder <atmulmuley@gmail.com>
2022-02-07 17:08:08 +0530

    added data parallelism examples

---
+++ ParallelProcessingCSharp/ParallelProcessingCSharp/DataParallelismExampleOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelProcessingCSharp
+{
+    public class DataParallelismExampleOne
+    {
+        public static string directoryName = @"C:/Users/atharvam/repos/ParallelProcessingCSharp/ParallelProcessingCSharp/ParallelProcessingCSharp";
+
+        public static void ExampleOne(string[] args)
+        {
+            long totalSize = 0;
+
+            if (!Directory.Exists(directoryName))
+            {
+                Console.WriteLine("The directory does not exist.");
+                return;
+            }
+
+            String[] files = Directory.GetFiles(directoryName);
+            Parallel.For(0, files.Length,
+                         index => {
+                             FileInfo fi = new FileInfo(files[index]);
+                             long size = fi.Length;
+                             Interlocked.Add(ref totalSize, size);
+                         });
+            Console.WriteLine("Directory '{0}':", directoryName);
+            Console.WriteLine("{0:N0} files, {1:N0} bytes", files.Length, totalSize);
+        }
+    }
+}
---
+++ ParallelProcessingCSharp/ParallelProcessingCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ParallelProcessingCSharp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataParallelismExampleOne.ExampleOne(new String[1]);
+            DataParallelismExampleTwo.ExampleTwo(new String[1]);
+            DataParallelismExampleThree.ExampleThree();
+            DataParallelismExampleFour.ExampleFour();
+            DataParallelismExampleFive.ExampleFive();
+            DataParallelismExampleSix.ExampleSix(new string[1]);
+        }
+    }
+}
